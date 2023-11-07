6511105b8604deea541631427f33e40fb63497c8
sajad-mhr <sajadmhr666@gmail.com>
2023-11-07 06:01:01 -0800

    handle routing

--- s06/s06.js
+++ s06/s06.js
@@ -1,9 +1,27 @@
 let http = require('http');
-http.createServer(reqHandler).listen(8080);
-let headers = { 'Content-Type': 'text/plain' }
-function reqHandler(req, res) {
+const PORT = 8080;
+http.createServer(reqHandler).listen(PORT);
+console.log(`Server On in Port ${PORT}`);
+let headers = { 'Content-Type': 'text/plain' };
+let funcs = {
+    sum: Sum,
+    power: Power
+}
+function Sum(res) {
+    console.log("Sum");
     res.writeHead(200, headers);
-    console.log(req.url);
-    res.write('url : ' + req.url);
+    res.write("SUM");
+}
+function Power(res) {
+    console.log("Power");
+    res.writeHead(200, headers);
+    res.write("Power");
+}
+
+function reqHandler(req, res) {
+    let path = req.url.split("/")[1]
+    if (path !== "favicon.ico") {
+        funcs[path](res);
+    }
     res.end();
 }
