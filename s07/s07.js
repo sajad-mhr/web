0329ddff78f461c9e39e4ce64c99f6e518882dfe
sajad-mhr <sajadmhr666@gmail.com>
2023-11-14 16:58:50 +0330

    fixed2

--- s07/s07.js
+++ s07/s07.js
@@ -1,76 +1,90 @@
 let http = require('http');
+let fs = require('fs');
 const PORT = 8080;
 http.createServer(reqHandler).listen(PORT);
-
 console.log(`Server On in Port ${PORT}`);
-
-
 let funcs = {
     home: homePageController,
-    about:aboutPageController,
-
+    about: aboutPageController,
 }
-
 let headers = {
     text: { 'Content-Type': 'text/plain' },
     html: { 'Content-Type': 'text/Html' }
 };
 
-let styles = `
-<style>
-div{
-    background-color:gray;
-    color:yellow;
-    width:200px;
-    height:200px;
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    font-size:1.2rem;
-    border-radius:14px
-    }
-</style>
-`
+
+// let styles = `
+// <style>
+// div{
+//     background-color:gray;
+//     color:yellow;
+//     width:200px;
+//     height:200px;
+//     display:flex;
+//     align-items:center;
+//     justify-content:center;
+//     font-size:1.2rem;
+//     border-radius:14px
+//     }
+// </style>
+// `
+// function homePageController(res) {
+//     res.writeHead(200, headers.html);
+//     let template = `
+//     <html>
+//         <head>
+//             <title>Home Page</title>
+//         </head>
+//         <body>
+//             ${styles}
+//             <div>
+//                 <h1>
+//                     Home
+//                 </h1>
+//             </div>
+//         </body>
+//     </html>
+//     `
+//     res.write(template);
+// }
+
+// function aboutPageController(res) {
+//     res.writeHead(200, headers.html);
+//     let template = `
+//     </html>
+//     <html>
+//         <head>
+//             <title>Home Page</title>
+//         </head>
+//         <body>
+//             ${styles}
+//             <div>
+//                 <h1>
+//                 About
+//                 </h1>
+//             </div>
+//         </body>
+//     </html>
+//     `
+//     res.write(template);
+// }
+
 
 function homePageController(res) {
-    res.writeHead(200, headers.html);
-    let template = `
-    <html>
-        <head>
-            <title>Home Page</title>
-        </head>
-        <body>
-            ${styles}
-            <div>
-                <h1>
-                    Home
-                </h1>
-            </div>
-        </body>
-    </html>
-    `
-    res.write(template);
+    fs.readFile('./pages/home.html', (error, data) => {
+        res.writeHead(200, headers.html);
+        res.write(data);
+        console.log(error);
+    })
+
 }
 
 function aboutPageController(res) {
-    res.writeHead(200, headers.html);
-    let template = `
-    </html>
-    <html>
-        <head>
-            <title>Home Page</title>
-        </head>
-        <body>
-            ${styles}
-            <div>
-                <h1>
-                About
-                </h1>
-            </div>
-        </body>
-    </html>
-    `
-    res.write(template);
+    fs.readFile('./pages/about.html', (error, data) => {
+        res.writeHead(200, headers.html);
+        res.write(data);
+        console.log(error);
+    })
 }
 
 
